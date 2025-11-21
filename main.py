3075182264dc7aec788eebd8b25e0cc659a33eae
Guilherme <gfernandoportelaoliveira@gmail.com>
2025-11-21 17:37:20 -0300

    Adicionando base.html

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask  import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def homepage():
   return render_template('index.html')
 
-@app.route("/usuario")
+@app.route("/perfil")
 def usuario():
-   return render_template('usuario.html')
+   return render_template('perfil.html')
 
 
 if __name__ == "__name__":
