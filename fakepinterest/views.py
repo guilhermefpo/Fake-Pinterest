ee374baed8ceb2284fc964d3b24d1737256aca30
Guilherme <gfernandoportelaoliveira@gmail.com>
2025-12-17 20:55:40 -0300

    Criado a funcionalidade de criar foto

--- fakepinterest/views.py
+++ fakepinterest/views.py
@@ -4,45 +4,72 @@ from flask import render_template, url_for, redirect
 from fakepinterest import app, database, bcrypt
 from fakepinterest.models import Usuario, Foto
 from flask_login import login_required, login_user, logout_user, current_user
-from fakepinterest.forms import FormLogin, FormCriarConta
+from fakepinterest.forms import FormLogin, FormCriarConta, FormFoto
+import os
+from werkzeug.utils import secure_filename
 
-
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def homepage():
-  formlogin = FormLogin()
-  if form_login.validate_on_submit():
-     usuario = Usuario.query.filter_by(email=form_login.email.data).first()
-     if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
-        login_user(usuario)
-        return redirect("perfil", usuario=usuario.username)
-  return render_template('index.html', form=formlogin)
+    formlogin = FormLogin()
 
-@app.route("/criarconta", methods=["GET","POST"])
+    if formlogin.validate_on_submit():
+        usuario = Usuario.query.filter_by(email=formlogin.email.data).first()
+
+        if usuario and bcrypt.check_password_hash(usuario.senha, formlogin.senha.data):
+            login_user(usuario)
+            return redirect(url_for("perfil", id_usuario=usuario.id))
+
+    return render_template("index.html", form=formlogin)
+
+
+@app.route("/criarconta", methods=["GET", "POST"])
 def criarconta():
-   formcriarconta = FormCriarConta()
-   if formcriarconta.validate_on_submit():
-      senha = bcrypt.generate_password_hash(formcriarconta.senha.data)
-      usuario = Usuario(username=formcriarconta.username.data, email=formcriarconta.email.data, senha=senha)
+    formcriarconta = FormCriarConta()
 
-      database.session.add(usuario)
-      database.session.commit()
-      login_user(usuario, remember=True)
-      return redirect("perfil", id_usuario=usuario.id)
-   return render_template("criarconta.html", form=formcriarconta)
+    if formcriarconta.validate_on_submit():
+        senha = bcrypt.generate_password_hash(
+            formcriarconta.senha.data
+        ).decode("utf-8")
 
-@app.route("/perfil/<id_usuario>")
+        usuario = Usuario(
+            username=formcriarconta.username.data,
+            email=formcriarconta.email.data,
+            senha=senha
+        )
+
+        database.session.add(usuario)
+        database.session.commit()
+        login_user(usuario, remember=True)
+
+        return redirect(url_for("perfil", id_usuario=usuario.id))
+
+    return render_template("criarconta.html", form=formcriarconta)
+
+
+@app.route("/perfil/<id_usuario>", methods=["GET", "POST"])
 @login_required
 def perfil(id_usuario):
-    if int(id_usuario) ==  int(current_user.id):
-         # O usuário ta vendo o perfil dele.
-         return render_template("perfil.html", usuario=current_user)
+    if int(id_usuario) == current_user.id:
+        form_foto = FormFoto()
+        if form_foto.validate_on_submit():
+            arquivo = form_foto.foto.data
+            nome_seguro = secure_filename(arquivo.filename)
+            # Salvar o arquivo na pasta fotos_post
+            # registrar o arquivo no banco de dados
+            caminho = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                              app.config["UPLOAD_FOLDER"], nome_seguro)
+            arquivo.save(caminho)
+            foto = Foto(imagem=nome_seguro, id_usuario=current_user.id)
+            database.session.add(foto)
+            database.session.commit()
+        return render_template("perfil.html", usuario=current_user, form=form_foto)
     else:
-      usuario = Usuario.query.get(int(id_usuario))
-      return render_template('perfil.html', usuario=usuario)
+        usuario = Usuario.query.get(int(id_usuario))
+        return render_template("perfil.html", usuario=usuario, form=None)
 
 
 @app.route("/logout")
 @login_required
 def logout():
-   logout_user()
-   return redirect(url_for("homepage"))
+    logout_user()
+    return redirect(url_for("homepage"))
