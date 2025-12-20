a543ccd2cc38b19453eeeb6acda90d878e628791
Guilherme <gfernandoportelaoliveira@gmail.com>
2025-12-19 23:36:22 -0300

    Adicionando variaveis app.config para um banco mais funcional

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -3,9 +3,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    os.getenv("DATABASE_URL") or "sqlite:///comunidade.db"
+)
 app.config["SECRET_KEY"] = "3ed06923cf5209673645"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
